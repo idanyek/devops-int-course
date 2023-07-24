764769da96cbdb090cf72b3e24b82304659c9cdf
idanyek <idan@yekutiel.net>
2023-07-24 18:25:30 +0300

    add leasson 04

---
+++ lesson_01_bassics/04_arithmetic_oprators.py
@@ -0,0 +1,27 @@
+number_1 = 6
+number_2 = 15
+
+
+print(f"{number_1} + {number_2} = {(number_1 + number_2)}")
+print(type(number_1 + number_2))
+print(f"{number_1} - {number_2} = {(number_1 - number_2)}")
+print(type(number_1 - number_2))
+print(f"{number_1} * {number_2} = {(number_1 * number_2)}")
+print(type(number_1 * number_2))
+print(f"{number_1} / {number_2} = {(number_1 / number_2)}")
+print(type(number_1 / number_2))
+print(f"{number_1} // {number_2} = {(number_1 // number_2)}")
+print(type(number_1 // number_2))
+print(f"{number_1} % {number_2} = {(number_1 % number_2)}")
+print(type(number_1 % number_2))
+print(f"{number_1} ** {number_2} = {(number_1 ** number_2)}")
+print(type(number_1 ** number_2))
+
+text_1 = "idan"
+text_2 = "yekutiel"
+
+print(f"{text_1} * {number_2} = {(text_1 * number_2)}")
+print(type(text_1 * number_2))
+
+print(f"{text_1} + {text_2} = {(text_1 + text_2)}")
+print(type(text_1 + text_2))

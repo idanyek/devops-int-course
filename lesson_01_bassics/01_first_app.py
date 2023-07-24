46a5a0b7134cc11794a8cb3751ba2202d0327d56
idanyek <idan@yekutiel.net>
2023-07-24 17:38:01 +0300

    before lession

--- lesson_01_bassics/01_first_app.py
+++
@@ -1,7 +0,0 @@
-def hello():
-    print("hello world")
-    print("new line")
-    print("remote line")
-
-if __name__ == '__main__':
-    hello()
--- lesson_01_bassics/03_from_github.py
+++
@@ -1,1 +0,0 @@
-print("this file created in github web")

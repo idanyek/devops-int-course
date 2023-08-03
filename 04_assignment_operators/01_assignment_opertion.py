ba24fb5524f71485be5f93214675d853f9cc08a7
idanyek <idan@yekutiel.net>
2023-08-03 18:13:30 +0300

    add 04/01

--- 04_assignment_operators/01_assignment_opertion.py
+++ 04_assignment_operators/01_assignment_opertion.py
@@ -1,4 +1,6 @@
 number = 10
+
+#+
 print(f"number: {number}")
 
 number = number + 1
@@ -7,5 +9,37 @@ print(f"number: {number}")
 number += 1
 print(f"number: {number}")
 
+#-
+number = number - 1
+print(f"number: {number}")
 
+number -= 1
+print(f"number: {number}")
+
+#*
+number = number * 20
+print(f"number: {number}")
+
+number *= 20
+print(f"number: {number}")
+
+#/
+number = number / 2.5
+print(f"number: {number}")
+
+number /= 2.8
+print(f"number: {number}")
+
+#%
+number = number % 10
+print(f"number: {number}")
+
+number %= 10
+print(f"number: {number}")
+
+#**
+number = number ** 10
+print(f"number: {number}")
+
+number **= 10
 print(f"number: {number}")

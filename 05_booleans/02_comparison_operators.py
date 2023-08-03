03556215fef0f8629392079202b12460f77a3424
idanyek <idan@yekutiel.net>
2023-08-03 19:55:54 +0300

    comparesion

---
+++ 05_booleans/02_comparison_operators.py
@@ -0,0 +1,20 @@
+
+print(f"5 > 8 = {5 < 8}") # true
+print(f"5 < 8 = {5 < 8}") # true
+
+print(f"4 < 4 = {4 < 4}") # false
+print(f"4 <= 4 = {4 <= 4}") # true
+print(f"4 >= 4 = {4 >= 4}") # true
+
+print(f"4 == 4 = {4 == 4}") # true
+print(f"4 == 5 = {4 == 5}") # false
+
+print(f"4 != 4 = {4 != 4}") # false
+print(f"4 != 5 = {4 != 5}") # true
+
+age = int(input("enter your age: "))
+
+print(f"{age} >= 18 = {age >= 18}")
+
+#odd or even
+print(f"{age} % 2 = 0 is {age % 2} - {age % 2 == 0}")

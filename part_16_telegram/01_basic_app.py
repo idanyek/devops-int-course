b8c256fa426aff313b9a930b35f352d4360f7235
idanyek <idan@yekutiel.net>
2024-01-12 11:03:31 +0200

    test

--- part_16_telegram/01_basic_app.py
+++ part_16_telegram/01_basic_app.py
@@ -5,16 +5,15 @@ from aiogram import Bot, Dispatcher, types
 # loading .env file
 load_dotenv()
 
-#getting token from env file
-bot = Bot(token=os.getenv("BOT_TOKEN"))
+# getting token from env file
+bot = Bot( token=os.getenv( "BOT_TOKEN" ) )
 dp = Dispatcher()
 
-async def main():
-    logging.basicConfig(level=logging.DEBUG)
-    await dp.start_polling(bot)
-    
 
+async def main():
+    logging.basicConfig( level=logging.DEBUG )
+    await dp.start_polling( bot )
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    asyncio.run( main() )
